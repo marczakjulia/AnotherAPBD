5fd8c8ae6addec0cee9b954e05a9b3740dd119f6
marczakjulia <s31153@pjwstk.edu.pl>
2025-03-27 19:15:25 +0100

    Changes

--- AnotherAPBD/DeviceWriteToFile.cs
+++ AnotherAPBD/DeviceWriteToFile.cs
@@ -1,18 +1,17 @@
 namespace AnotherAPBD
-{
-    public static class DeviceWriteToFile
+{ public static class DeviceWriteToFile
     {
         /// <summary>
         /// Saves the provided list of devices to the specified file.
         /// </summary>
-        public static void SaveDevices(string outputPath, IEnumerable<Device> devices)
-        {
-            var linesList = new List<string>();
-            foreach (Device device in devices)
-            {
+        public static void SaveDevices(string outputPath, List<Device> devices)
+        { 
+            var linesList = new List<string>(); 
+            foreach (Device device in devices) 
+            { 
                 linesList.Add(device.ToSavingString());
-            }
+            } 
             File.WriteAllLines(outputPath, linesList.ToArray());
+                }
+            }
         }
-    }
-}
